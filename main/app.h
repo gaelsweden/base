ce63e2155fb5e7ec11e9f421ccba86c37af5c181
Administrateur Local <Mod_loc@D46-P02.D-Electronique.local>
2023-05-24 15:10:13 +0200

    Test

---
+++ main/app.h
@@ -0,0 +1,52 @@
+/**
+ * @file app.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-05-17
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********** Application parameters *********************************************/
+#define APP_FLASH_LED_PIN               ((gpio_num_t)(CONFIG_FLASH_LED_GPIO))
+#define APP_SENDING_INTERVAL_MS         (10000)
+#define APP_SENDING_MESSAGE_STR         ("LoRa Base Station")
+#define APP_TAG_STR                     ("[$BASE-APP$]:")
+
+#define APP_LORA_PROBE_NB               (10)
+
+/*********** LoRa host station addresses definition *****************************/
+#define APP_LORA_HOST_ADDRESS           (0x10)  /* address of this module       */
+#define APP_LORA_REMOTE_ADDRESS         (0x15)  /* address of the remote module */
+#define APP_LORA_BCAST_ADDRESS          (0xff)  /* address for broadcasting     */
+
+/*********** LoRa module parameters *********************************************/
+#define APP_LORA_CARRIER_FREQUENCY_HZ   (868E6)
+#define APP_LORA_SPREADING_FACTOR       (8)
+#define APP_LORA_BANDWIDTH_FREQUENCY    (62.5E3)
+#define APP_LORA_CODING_RATE            (6)
+
+/*********** LoRa module requests *********************************************/
+#define APP_LORA_REQUEST_ADDRESS        (15)
+
+/********************************************************************************/
+
+typedef enum e_bool{
+    FALSE = 0,
+    TRUE
+}t_bool;
+
+
+void AppInit(void);
+void AppRun(void);
+
+
+#ifdef __cplusplus
+}
+#endif
